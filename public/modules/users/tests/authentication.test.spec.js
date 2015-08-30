3c3bae2ff2fdafdfbce0e344350caffd11464c96
Timothy Voice <timsethvoice@gmail.com>
2015-08-30 16:00:45 -0400

    user testing scaffold passing

--- public/modules/users/tests/authentication.test.spec.js
+++ public/modules/users/tests/authentication.test.spec.js
@@ -6,34 +6,32 @@ describe('User Authentication', function() {
     password: 'password'
   }
 
-  it('should sign up a new user', function() {
-    // Load the AngularJS homepage.
-    browser.get(baseUrl + '/signup');
-
-    // Find the element with ng-model matching 'yourName' - this will
-    // find the <input type="text" ng-model="yourName"/> element - and then
-    // type 'Julie' into it.
-    element(by.css('form [id*=input_email]')).sendKeys(userCredentials.email);
-    element(by.css('form [id*=password_password]')).sendKeys(userCredentials.password);
-    element(by.css('form button[type=submit]')).click();
-    // Assert that the text element has the expected value.
-    // Protractor patches 'expect' to understand promises.
+  it('should sign in an existing user', function() {
+    // user sign in
+    browser.get(baseUrl + '/signin');
+    element(by.css('[id*=user-email]')).sendKeys(userCredentials.email);    
+    element(by.css('[id*=user-password]')).sendKeys(userCredentials.password);    
+    element(by.css('button[type=submit]')).click();  
     expect(browser.getLocationAbsUrl())
-        .toBe('/');
-    // expect($scope.authentication.user.email).toEqual('tim@mail.com');
+        .toBe('/');    
+  });
+
+  it('should update a user profile', function() {
+    // user sign in
+    browser.get(baseUrl + '/signin');
+    element(by.css('[id*=user-email]')).sendKeys(userCredentials.email);    
+    element(by.css('[id*=user-password]')).sendKeys(userCredentials.password);    
+    element(by.css('button[type=submit]')).click();  
+    expect(browser.getLocationAbsUrl())
+        .toBe('/');  
+    // update user profile
+    browser.get(baseUrl + '/settings/profile');
+    element(by.buttonText('Save Profile')).click();
   });
 
   afterEach(function(){
-    // user sign in
-    // browser.get(baseUrl + '/signin');
-    element(by.css('[id*=input_email]')).sendKeys(userCredentials.email);    
-    element(by.css('[id*=password_password]')).sendKeys(userCredentials.password);    
-    element(by.css('button[type=submit]')).click();    
-
-    // delete user
-    browser.get(baseUrl + '/settings/profile')
-    element(by.css('button.delete')).click(); 
-    expect(browser.getLocationAbsUrl())
-        .toBe('/');   
+    browser.get(baseUrl + '/');
+    element(by.css('.user-signout')).click();
   })
+
 });
