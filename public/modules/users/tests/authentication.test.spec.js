c415a4f91934734bc5b3e091ac8a509f849394de
Timothy Voice <timsethvoice@gmail.com>
2015-09-02 17:29:06 -0400

    signin test

--- public/modules/users/tests/authentication.test.spec.js
+++ public/modules/users/tests/authentication.test.spec.js
@@ -6,32 +6,50 @@ describe('User Authentication', function() {
     password: 'password'
   }
 
+  var signin = function() {
+    browser.get(baseUrl + '/');
+    element(by.css('a.signin')).click();
+    browser.wait(EC.visibilityOf($('#user-signin-email')), 2000);
+    element(by.css('#user-signin-email')).sendKeys(userCredentials.email);    
+    element(by.css('#user-signin-password')).sendKeys(userCredentials.password);       
+    element(by.css('button#signin')).click(); 
+  }
+
+  var EC = protractor.ExpectedConditions;
+
   it('should sign in an existing user', function() {
     // user sign in
-    browser.get(baseUrl + '/signin');
-    element(by.css('[id*=user-email]')).sendKeys(userCredentials.email);    
-    element(by.css('[id*=user-password]')).sendKeys(userCredentials.password);    
-    element(by.css('button[type=submit]')).click();  
+    browser.get(baseUrl + '/');
+    element(by.css('a.signin')).click();
+    browser.wait(EC.visibilityOf($('#user-signin-email')), 2000);
+    element(by.css('#user-signin-email')).sendKeys(userCredentials.email);    
+    element(by.css('#user-signin-password')).sendKeys(userCredentials.password);       
+    element(by.css('button#signin')).click();  
     expect(browser.getLocationAbsUrl())
         .toBe('/');    
   });
 
   it('should update a user profile', function() {
-    // user sign in
-    browser.get(baseUrl + '/signin');
-    element(by.css('[id*=user-email]')).sendKeys(userCredentials.email);    
-    element(by.css('[id*=user-password]')).sendKeys(userCredentials.password);    
-    element(by.css('button[type=submit]')).click();  
+    browser.get(baseUrl + '/');
+    element(by.css('a.signin')).click();
+    browser.wait(EC.visibilityOf($('#user-signin-email')), 2000);
+    element(by.css('#user-signin-email')).sendKeys(userCredentials.email);    
+    element(by.css('#user-signin-password')).sendKeys(userCredentials.password);       
+    element(by.css('button#signin')).click();  
     expect(browser.getLocationAbsUrl())
-        .toBe('/');  
+        .toBe('/'); 
+    browser.wait(EC.invisibilityOf($('#signout')), 2000);
+    element(by.css('#signout')).click();
     // update user profile
-    browser.get(baseUrl + '/settings/profile');
-    element(by.buttonText('Save Profile')).click();
+    // browser.get(baseUrl + '/settings/profile');
+    // element(by.buttonText('Save Profile')).click();
+
   });
 
   afterEach(function(){
     browser.get(baseUrl + '/');
-    element(by.css('.user-signout')).click();
+    browser.wait(EC.visibilityOf($('#signout')), 2000);
+    element(by.css('#signout')).click();
   })
 
 });
